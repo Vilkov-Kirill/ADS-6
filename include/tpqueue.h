2ce10e58d205132624d84d2e21abbf79f4b03c5a
Vilkov-Kirill <99217802+Vilkov-Kirill@users.noreply.github.com>
2022-05-13 20:52:23 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,3 +1,4 @@
+// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
@@ -5,7 +6,7 @@
 
 template<typename T, int size>
 class TPQueue {
-private:
+ private:
     T* arr = new T[size+1];
     int top;
     int checkInd(int ind) {
@@ -13,35 +14,41 @@ private:
         while (ind > size) ind -= size;
         return ind;
     }
-public:
-    TPQueue(): top(0) {};
+
+ public:
+    TPQueue(): top(0) {}
     ~TPQueue() { delete[] arr; }
 
     T pop() {
         return arr[checkInd(top++)];
     }
 
-    void push(const T &data) {		// O(n)
-        if (arr[checkInd(top)].prior == -1) arr[checkInd(top)] = data;
-        else {
+    void push(const T &data) {    // O(n)
+        if (arr[checkInd(top)].prior == -1) {
+            arr[checkInd(top)] = data;
+        } else {
             int i = top;
             int flag = 0;
-            while (data.prior <= arr[checkInd(i)].prior) {		// for less or same prior
+            // for less or same prior
+            while (data.prior <= arr[checkInd(i)].prior) {
                 i++;
                 flag = 1;
             }
             if (flag == 1) {
-                if (arr[checkInd(i)].prior == -1) arr[checkInd(i)] = data;
-                else {
+                if (arr[checkInd(i)].prior == -1) {
+                    arr[checkInd(i)] = data;
+                } else {
                     int k = i+1;
-                    while (arr[checkInd(k)].prior != -1 && checkInd(k) != checkInd(top)) k++;
+                    while (arr[checkInd(k)].prior != -1 &&
+                        checkInd(k) != checkInd(top)) k++;
                     if (checkInd(k) == checkInd(top)) top++;
                     arr[checkInd(k)] = arr[checkInd(k - 1)];
                     arr[checkInd(i)] = data;
                 }
                 return;
             }
-            arr[checkInd(--top)] = data;	    // for higher prior
+            // for higher prior
+            arr[checkInd(--top)] = data;
         }
     }
 };
